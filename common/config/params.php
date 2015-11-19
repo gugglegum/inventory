f83209fe934053e0444ce7b25a6209ef00db486b
Paul Melekhov <gugglegum@gmail.com>
2015-11-19 17:32:49 +0500

    Добавлены конфиг-параметры для сохранения загруженных фотографий предметов

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,16 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    'photos' => [
+        'storagePath' => dirname(dirname(__DIR__)) . '/photos',
+        'storageRelativeUrl' => 'photos',
+        'resize' => [
+            'width' => 1024,
+            'height' => 1024,
+            'antiAliasing' => true,
+            'upscale' => false,
+            'crop' => false,
+            'quality' => 90,
+        ],
+    ],
 ];
