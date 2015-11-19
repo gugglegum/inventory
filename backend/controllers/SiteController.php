cc1eb1ad845f07c369ce925f4b401f2f3b2e81fc
Paul Melekhov <gugglegum@gmail.com>
2015-11-19 17:45:22 +0500

    Сделан редирект с главной страницы на страницу контроллера Items

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -55,7 +55,7 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->redirect(['items/index']);
     }
 
     public function actionLogin()
