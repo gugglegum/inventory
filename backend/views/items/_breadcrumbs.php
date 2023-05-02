c9af3cfa07a03b3c586c5cf7a65081bf04d253f2
Paul Melekhov <gugglegum@gmail.com>
2023-05-03 03:46:19 +0400

    Исправлены "хлебные крошки", которые могли отображаться некорректно, если в parentId из POST было загружено (но ещё не сохранено в БД) какое-то некорректное значение

--- backend/views/items/_breadcrumbs.php
+++ backend/views/items/_breadcrumbs.php
@@ -6,17 +6,20 @@ use yii\helpers\Html;
 
 $this->params['breadcrumbs'][] = ['label' => 'Предметы', 'url' => ['items/index']];
 $path = [];
-$tmpItem = $model;
-while ($tmpItem) {
-    $path[] = [
-        'label' => $tmpItem->name,
-        'url' => ['items/view', 'id' => $tmpItem->id],
-        'template' => empty($path)
-            ? '<li class="active">{link}<sup style="margin-left: 3px">#' . Html::encode($tmpItem->id) . "</sup></li>\n"
-            : '<li>{link}<sup style="margin-left: 3px; color: #777">#' . Html::encode($tmpItem->id) . "</sup></li>\n",
-    ];
-    $tmpItem = $tmpItem->parent;
-}
-for ($i = count($path) - 1; $i >= 0; $i--) {
-    $this->params['breadcrumbs'][] = $path[$i];
+if ($model) {
+    $tmpItem = clone $model;
+    $tmpItem->refresh(); // Предотвращает неправильную цепочку родителей, когда в parentId из POST был загружен какой-то невалидный ID
+    while ($tmpItem) {
+        $path[] = [
+            'label' => $tmpItem->name,
+            'url' => ['items/view', 'id' => $tmpItem->id],
+            'template' => empty($path)
+                ? '<li class="active">{link}<sup style="margin-left: 3px">#' . Html::encode($tmpItem->id) . "</sup></li>\n"
+                : '<li>{link}<sup style="margin-left: 3px; color: #777">#' . Html::encode($tmpItem->id) . "</sup></li>\n",
+        ];
+        $tmpItem = $tmpItem->parent;
+    }
+    for ($i = count($path) - 1; $i >= 0; $i--) {
+        $this->params['breadcrumbs'][] = $path[$i];
+    }
 }
