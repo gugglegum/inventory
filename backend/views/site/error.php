962c917ac0139d85b96a1d35ddb60fbe9470c507
Paul Melekhov <gugglegum@gmail.com>
2023-07-31 05:10:35 +0400

    В мини-форму поиска предмета по ID добавлен ID текущего запрошенного, но не найденного предмета на странице 404

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -8,6 +8,13 @@
 use yii\helpers\Html;
 
 $this->title = $name;
+
+// Little hack to display item ID in IdForm on the error page
+if (preg_match('|/items/view\?id=([^&]+)|', $_SERVER['REQUEST_URI'], $m)) {
+    $itemId = $m[1];
+} else {
+    $itemId = '';
+}
 ?>
 <div class="site-error">
 
@@ -26,7 +33,7 @@ $this->title = $name;
 
         <div id="idFormWrapper">
             <?= $this->render('/items/_idForm', [
-                'id' => '',
+                'id' => $itemId,
                 'prevItem' => null,
                 'nextItem' => null,
             ]) ?>
