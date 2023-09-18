7b05793275d159d377508fb8de8d7779cbeafd66
Paul Melekhov <gugglegum@gmail.com>
2023-09-19 03:10:07 +0400

    В форме быстрого перехода на предмет по ID, поле с ID сделано числовым и немного увеличено

--- backend/views/items/_idForm.php
+++ backend/views/items/_idForm.php
@@ -16,7 +16,7 @@ $this->registerCssFile('@web/css/search-form.css', ['appendTimestamp' => true], 
     <?php if ($item) { ?><a href="<?= Html::encode(Url::to($item->parentId !== null ? ['items/view', 'id' => $item->parentId] : ['items/index'])) ?>"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></a><?php } else { ?><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span><?php } ?>
     <?php if ($prevItem) { ?><a href="<?= Html::encode(Url::to(['items/view', 'id' => $prevItem->id])) ?>"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span></a><?php } else { ?><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span><?php } ?>
     <label for="inputId">#</label>
-    <input type="text" name="id" id="inputId" value="<?= Html::encode($id) ?>">
+    <input type="number" pattern="\d*" name="id" id="inputId" value="<?= Html::encode($id) ?>">
     <button type="submit"><span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span></button>
     <?php if ($nextItem) { ?><a href="<?= Html::encode(Url::to(['items/view', 'id' => $nextItem->id])) ?>"><span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span></a><?php } else { ?><span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><?php } ?>
 </form>
