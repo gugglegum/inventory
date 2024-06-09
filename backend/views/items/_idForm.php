828d979294bd2e1403784c81077145fc0de2e241
Paul Melekhov <gugglegum@gmail.com>
2024-06-09 18:06:47 +0400

    Улучшена навигация по формам поиска при помощи кнопки TAB на клавиатуре

--- backend/views/items/_idForm.php
+++ backend/views/items/_idForm.php
@@ -11,12 +11,14 @@ use yii\helpers\Url;
 
 $this->registerCssFile('@web/css/search-form.css', ['appendTimestamp' => true], 'search-form');
 
+$tabIndex = 4;
+
 ?>
 <form action="<?= Html::encode(Url::to(['items/view'])) ?>" id="idForm">
-    <?php if ($item) { ?><a href="<?= Html::encode(Url::to($item->parentId !== null ? ['items/view', 'id' => $item->parentId] : ['items/index'])) ?>"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></a><?php } else { ?><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span><?php } ?>
-    <?php if ($prevItem) { ?><a href="<?= Html::encode(Url::to(['items/view', 'id' => $prevItem->id])) ?>"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span></a><?php } else { ?><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span><?php } ?>
+    <?php if ($item) { ?><a href="<?= Html::encode(Url::to($item->parentId !== null ? ['items/view', 'id' => $item->parentId] : ['items/index'])) ?>" tabindex="<?= $tabIndex++ ?>"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></a><?php } else { ?><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span><?php } ?>
+    <?php if ($prevItem) { ?><a href="<?= Html::encode(Url::to(['items/view', 'id' => $prevItem->id])) ?>" tabindex="<?= $tabIndex++ ?>"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span></a><?php } else { ?><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span><?php } ?>
     <label for="inputId">#</label>
-    <input type="number" pattern="\d*" name="id" id="inputId" value="<?= Html::encode($id) ?>">
-    <button type="submit"><span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span></button>
-    <?php if ($nextItem) { ?><a href="<?= Html::encode(Url::to(['items/view', 'id' => $nextItem->id])) ?>"><span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span></a><?php } else { ?><span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><?php } ?>
+    <input type="number" pattern="\d*" name="id" id="inputId" value="<?= Html::encode($id) ?>" tabindex="<?= $tabIndex++ ?>">
+    <button type="submit" tabindex="<?= $tabIndex++ ?>"><span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span></button>
+    <?php if ($nextItem) { ?><a href="<?= Html::encode(Url::to(['items/view', 'id' => $nextItem->id])) ?>" tabindex="<?= $tabIndex++ ?>"><span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span></a><?php } else { ?><span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><?php } ?>
 </form>
--- backend/views/items/_searchForm.php
+++ backend/views/items/_searchForm.php
@@ -12,16 +12,17 @@ use yii\helpers\Url;
 
 $this->registerCssFile('@web/css/search-form.css', ['appendTimestamp' => true], 'search-form');
 
+$tabIndex = 1;
 ?>
 <form action="<?= Html::encode(Url::to($containerSearch ? ['items/search-container'] : ['items/search'])) ?>" id="itemSearchForm">
     <div>
         <label for="inputQuery">Я ищу:</label>
-        <input id="inputQuery" type="text" name="q" value="<?= Html::encode($query) ?>" />
-        <input type="submit" name="" value="Найти" />
+        <input id="inputQuery" type="text" name="q" value="<?= Html::encode($query) ?>" tabindex="<?= $tabIndex ?>" />
+        <input type="submit" name="" value="Найти" tabindex="<?= $tabIndex + 2 ?>" />
     </div>
 <?php if ($showExtraOptions) { ?>
     <div id="divExtraSearchOptions">
-        <input type="checkbox" id="chkSearchInside" name="c" value="<?= $containerId ?>"<?= $searchInside ? ' checked' : ''?> /><label for="chkSearchInside">Искать внутри</label>
+        <input type="checkbox" id="chkSearchInside" name="c" value="<?= $containerId ?>"<?= $searchInside ? ' checked' : ''?> tabindex="<?= $tabIndex + 1 ?>" /><label for="chkSearchInside">Искать внутри</label>
     </div>
 <?php } ?>
 </form>
