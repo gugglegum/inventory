99b7cd05fb8200a2242f87bb1872af462a463121
Paul Melekhov <gugglegum@gmail.com>
2024-06-26 04:13:18 +0400

    Исправлена проблема с форматированием описания предмета. Было избыточное экранирование HTML-спецсимволов.

--- backend/views/items/view.php
+++ backend/views/items/view.php
@@ -22,9 +22,23 @@ $this->registerJsFile('@web/js/item-view.js', ['appendTimestamp' => true, 'depen
 
 $this->render('//_fancybox'); // Подключение jQuery-плагина Fancybox (*.js + *.css)
 
-$description = trim($model->description) !== '' ? TextFormatter::render(TextFormatter::parse(preg_replace('/(?<![\r\n])\r\n?(?![\r\n])/u', "\n<br>\n", Html::encode($model->description)))) : '<em>Нет описания</em>';
-// Выделяем ссылками упоминания ID предметов вида "#1234"
-$description = preg_replace_callback('/(?<=[\s.,;()<>{}\[\]]|^)(#(\d+))(?=[\s.,;()<>{}\[\]]|$)/', function(array $matches) { return '<a href="' . Html::encode(Url::to(['items/view', 'id' => $matches[2]])) . '">' . $matches[1] . '</a>'; }, $description);
+$description = trim($model->description);
+if ($description !== '') {
+    //$description = Html::encode($description);
+    //$description = preg_replace('/(?<![\r\n])\r\n?(?![\r\n])/u', "\n<br>\n", $description);
+    $description = TextFormatter::parse($description);
+    $description = TextFormatter::render($description);
+
+    // Выделяем ссылками упоминания ID предметов вида "#1234"
+    $description = preg_replace_callback(
+        '/(?<=[\s.,;()<>{}\[\]]|^)(#(\d+))(?=[\s.,;()<>{}\[\]]|$)/',
+        function(array $matches) {
+            return '<a href="' . Html::encode(Url::to(['items/view', 'id' => $matches[2]])) . '">' . $matches[1] . '</a>';
+        },
+        $description);
+} else {
+    $description = '<em>Нет описания</em>';
+}
 
 ?>
 <div id="item-view">
