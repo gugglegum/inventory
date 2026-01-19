956761b107af2c13eeb4f5b0c4554a6f1f199df3
Paul Melekhov <gugglegum@gmail.com>
2026-01-19 17:34:50 +0500

    Убран ненужный более код.

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -9,12 +9,6 @@ use yii\helpers\Html;
 
 $this->title = $name;
 
-// Little hack to display item ID in IdForm on the error page
-if (preg_match('|/items/view\?id=([^&]+)|', $_SERVER['REQUEST_URI'], $m)) {
-    $itemId = $m[1];
-} else {
-    $itemId = '';
-}
 ?>
 <div class="site-error">
 
